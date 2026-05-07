54a0fd4209a45da386f7786990057fd56e7f42f9
Felacc <98856469+Felacc@users.noreply.github.com>
2026-05-06 19:44:11 -0700

    feat: add grid size button

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,14 +1,17 @@
 // global vars
 const gridContainer = document.querySelector("#grid");
-const gridSize = 16;
+let gridSize = 16;
 
 // main
 function main() {
     createGrid(gridSize);
+    activateButton();
 }
 
 // create grid  
 function createGrid (gridSize) {
+    // clear grid
+    gridContainer.innerHTML = "";
     // rows
     for (i = 0; i < gridSize; i++) {
         const gridRow = document.createElement("div");
@@ -25,17 +28,25 @@ function createGrid (gridSize) {
 }
 
 function sketchable(div) {
-    div.addEventListener("mouseenter", (e) => {
+    div.addEventListener("mouseenter", () => {
         if (div.style.backgroundColor == "black") {
             div.style.backgroundColor = "white";
         } else {
             div.style.backgroundColor = "black";
         }
-
         
     });
 }
 
+function activateButton() {
+    btn = document.querySelector("#gridSizeBtn");
+    btn.addEventListener("click", () => {
+        gridSize = prompt("How big should the grid be?")
+        createGrid(gridSize);
+    })
+
+    
+}
 // call main
 main();
  
