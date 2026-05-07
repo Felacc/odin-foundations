6de398c13f09e24334490570097fa2f9456bd1bc
Felacc <98856469+Felacc@users.noreply.github.com>
2026-05-06 18:59:15 -0700

    feat: add hover effect

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,28 +1,41 @@
 // global vars
-const gridContainer = document.querySelector("#grid")
-const gridSize = 16
+const gridContainer = document.querySelector("#grid");
+const gridSize = 16;
 
 // main
 function main() {
-    createGrid(gridSize)
+    createGrid(gridSize);
 }
 
 // create grid  
 function createGrid (gridSize) {
     // rows
     for (i = 0; i < gridSize; i++) {
-        const gridRow = document.createElement("div")
-        gridRow.classList.add("gridRow", "flex")
-        gridContainer.appendChild(gridRow)
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("gridRow", "flex");
+        gridContainer.appendChild(gridRow);
         // columns
         for (j = 0; j < gridSize; j++) {
-            const gridCol = document.createElement("div")
-            gridCol.classList.add("gridCol")
-            gridRow.appendChild(gridCol)
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("gridSquare");
+            sketchable(gridSquare);
+            gridRow.appendChild(gridSquare);
         }
     }
 }
 
+function sketchable(div) {
+    div.addEventListener("mouseenter", (e) => {
+        if (div.style.backgroundColor == "black") {
+            div.style.backgroundColor = "white";
+        } else {
+            div.style.backgroundColor = "black";
+        }
+
+        
+    });
+}
+
 // call main
-main()
+main();
  
